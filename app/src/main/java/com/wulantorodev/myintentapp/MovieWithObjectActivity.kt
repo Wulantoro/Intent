528e2d6c10b1620d1e87d2764544d4bc841f444d
wulantoro <nugrahawulantoro@gmail.com>
2021-10-05 22:07:12 +0700

    first commit

---
+++ app/src/main/java/com/wulantorodev/myintentapp/MovieWithObjectActivity.kt
@@ -0,0 +1,23 @@
+package com.wulantorodev.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MovieWithObjectActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_PERSON = "extra_person"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_with_object)
+
+        val tvObject: TextView = findViewById(R.id.tv_object_received)
+
+        val person = intent.getParcelableExtra<Person>(EXTRA_PERSON) as Person
+        val text = "Name ${person.name}, Age : ${person.age}, email : ${person.email}, City : ${person.city}"
+        tvObject.text = text
+    }
+}
